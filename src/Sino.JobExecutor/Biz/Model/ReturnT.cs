b6beda8781236a84193d7b2cb04e25fa9ee8f362
姚正发 <976691141@qq.com>
2020-02-18 17:03:59 +0800

    暂存

---
+++ src/Sino.JobExecutor/Biz/Model/ReturnT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.JobExecutor.Biz.Model
+{
+    public class ReturnT<T> where T : class
+    {
+        public static int SUCCESS_CODE = 200;
+        public static int FAIL_CODE = 500;
+
+
+
+        public ReturnT() { }
+
+        public ReturnT(int code, string msg)
+        {
+            Code = code;
+            Msg = msg;
+        }
+
+        public int Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public T Content { get; set; }
+    }
+}
