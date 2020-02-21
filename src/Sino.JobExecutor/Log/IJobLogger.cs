455b9bad0820a3833dec0b553fd0e89e6f564202
姚正发 <976691141@qq.com>
2020-02-21 10:50:23 +0800

    添加忽略的文件

---
+++ src/Sino.JobExecutor/Log/IJobLogger.cs
@@ -0,0 +1,26 @@
+﻿using Sino.JobExecutor.Biz.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.JobExecutor
+{
+    /// <summary>
+    /// 任务日志接口
+    /// </summary>
+    public interface IJobLogger
+    {
+        /// <summary>
+        /// 设置日志文件
+        /// </summary>
+        void SetLogFile(long logTime, int logId);
+
+        void Log(string pattern, params object[] format);
+
+        void LogError(Exception ex);
+
+        LogResult ReadLog(long logTime, int logId, int fromLine);
+
+        void LogSpecialFile(long logTime, int logId, string pattern, params object[] format);
+    }
+}
---
+++ src/Sino.JobExecutor/Log/JobLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sino.JobExecutor.Biz.Model;
+
+namespace Sino.JobExecutor.Log
+{
+    /// <summary>
+    /// 任务日志，主要提供计算任务记录日志并供调度中心拉取
+    /// </summary>
+    public class JobLogger : IJobLogger
+    {
+        public void Log(string pattern, params object[] format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogSpecialFile(long logTime, int logId, string pattern, params object[] format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogResult ReadLog(long logTime, int logId, int fromLine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLogFile(long logTime, int logId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
